4cec0a81f4112b8437c8c14a31595f51482b05a8
PaulNafureanu <paul.nafureanu@gmail.com>
2023-02-27 18:24:24 +0200

    Refactor input and input group styles

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ function toggleExtension(extensionId) {
     extension.classList.add("collapsible__extension--extended");
   }
 }
+
+/* CSS Function Rules */
+
+function styleInputGroupOnFocus(groupId, inputId, btnId) {
+  let groupEl = document.getElementById(groupId);
+  let inputEl = document.getElementById(inputId);
+  let btnEl = document.getElementById(btnId);
+
+  inputEl.addEventListener("focusin", () => {
+    groupEl.style.border = "1px solid rgba(0, 0, 0, 0.1)";
+    groupEl.style.boxShadow = "0 0 50px 50px rgba(0, 0, 0, 0.15)";
+  });
+  inputEl.addEventListener("focusout", () => {
+    groupEl.style.border = "1px solid transparent";
+    groupEl.style.boxShadow = "none";
+  });
+
+  btnEl.addEventListener("click", () => {
+    groupEl.style.border = "1px solid transparent";
+    groupEl.style.boxShadow = "none";
+  });
+}
